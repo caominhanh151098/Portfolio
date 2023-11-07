d108ac97fa1af3534dec531e3da36bd90b1c8221
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-11-07 10:19:30 +0700

    push portfolio

---
+++ assets/js/portfolio-details.js
@@ -0,0 +1,58 @@
+const portfolio_category = [
+    {
+        category: 'Thiết kế web',
+        nameProject: 'Web thuê nhà nghỉ Airbnb',
+        time: '08/2023 - 10/2023',
+        linkProject : 'https://house-for-rent-psi.vercel.app/',
+        linkGithubServer: 'https://github.com/caominhanh151098/HouseForRent_Project',
+        linkGithubClient: 'https://github.com/caominhanh151098/HouseForRent_React',
+        description: ''
+    },
+    {
+        category: 'Thiết kế web',
+        nameProject: 'Web xem phim',
+        time: '07/2023 - 8/2023',
+        linkGithubServer: 'https://github.com/caominhanh151098/CaseStudy_MovieProject',
+        description: ''
+    },
+    {
+        category: 'Thiết kế web Game',
+        nameProject: 'Minigame Megaman X',
+        time: '02/2023 - 03/2023',
+        linkProject : 'https://megaman-x.vercel.app/',
+        linkGithubServer: 'https://github.com/caominhanh151098/CaseStudy_MegaManX',
+        description: ''
+    }
+]
+
+const portfolio_imgs = [
+    [
+        "assets/img/portfolio/house/Capture.png",
+        "assets/img/portfolio/house/Capture(1).png",
+        "assets/img/portfolio/house/Capture(2).png",
+        "assets/img/portfolio/house/Capture(3).png",
+        "assets/img/portfolio/house/Capture(4).png",
+        "assets/img/portfolio/house/Capture(5).png",
+        "assets/img/portfolio/house/Capture(6).png",
+        "assets/img/portfolio/house/Capture(7).png",
+        "assets/img/portfolio/house/Capture(8).png",
+        "assets/img/portfolio/house/Capture(9).png"
+    ],
+    [
+        "assets/img/portfolio/film/Capture.png",
+        "assets/img/portfolio/film/Capture1.png",
+        "assets/img/portfolio/film/Capture2.png",
+        "assets/img/portfolio/film/Capture3.png",
+        "assets/img/portfolio/film/Capture4.png",
+        "assets/img/portfolio/film/Capture5.png",
+        "assets/img/portfolio/film/Capture6.png",
+        "assets/img/portfolio/film/Capture7.png",
+    ],
+    [
+        "assets/img/portfolio/megaman/Capture.png",
+        "assets/img/portfolio/megaman/Capture1.png",
+        "assets/img/portfolio/megaman/Capture2.png",
+        "assets/img/portfolio/megaman/Capture3.png",
+        "assets/img/portfolio/megaman/Capture4.png",
+    ]
+]
